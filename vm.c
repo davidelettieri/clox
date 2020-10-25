637fc04dfa5d604e665084c2697d8479d0ca8afe
Davide Lettieri <davide.lettieri@gmail.com>
2020-10-25 18:06:41 +0000

    up to OP_NEGATE

--- vm.c
+++ vm.c
@@ -5,12 +5,30 @@
 
 VM vm;
 
+static void resetStack()
+{
+    vm.stackTop = vm.stack;
+}
+
 void initVM()
 {
+    resetStack();
 }
 
 void freeVM()
 {
+}
+
+void push(Value value)
+{
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop()
+{
+    vm.stackTop--;
+    return *vm.stackTop;
 }
 
 static InterpretResult run()
@@ -21,18 +39,35 @@ static InterpretResult run()
     for (;;)
     {
 #ifdef DEBUG_TRACE_EXECUTION
+        printf("          ");
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++)
+        {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
         disassembleInstruction(vm.chunk, ((int)(vm.ip - vm.chunk->code)));
 #endif
         uint8_t instruction;
         switch (instruction = READ_BYTE())
         {
         case OP_CONSTANT:
+        {
             Value constant = READ_CONSTANT();
+            push(constant);
             printValue(constant);
             printf("\n");
             break;
+        }
+        case OP_NEGATE:
+            push(-pop());
+            break;
         case OP_RETURN:
+        {
+            printValue(pop());
+            printf("\n");
             return INTERPRET_OK;
+        }
         default:
             break;
         }
