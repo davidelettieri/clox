918029f823e4639bbcfdf9865857aa0961cb7fa8
Davide Lettieri <davide.lettieri@gmail.com>
2020-10-27 08:03:17 +0000

    up to string()

--- compiler.c
+++ compiler.c
@@ -7,4 +7,26 @@
 void compile(const char *source)
 {
     initScanner(source);
+    int line = -1;
+
+    for (;;)
+    {
+        Token token = scanToken();
+        if (token.line != line)
+        {
+            printf("%4d ", token.line);
+            line = token.line;
+        }
+        else
+        {
+            printf("   | ");
+        }
+
+        printf("%d2 '%.*s'\n", token.type, token.length, token.start);
+
+        if (token.type == TOKEN_EOF)
+        {
+            break;
+        }
+    }
 }
